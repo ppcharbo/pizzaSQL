b7731866e0ed79997b86a5dea7ecd2039b7bdfcd
Pierre Paul <pierrepaul.charbonnier@gmail.com>
2022-10-11 18:25:59 +0200

    database add informations;

---
+++ app/MySQL/CommandeLineExecution.sql
@@ -0,0 +1,80 @@
+insert into ingredient(id, ingredient,price) values ('1','pomodoro','100');
+insert into ingredient(id, ingredient,price) values ('2','mozzarella','200');
+insert into ingredient(id, ingredient,price) values ('3','prosciutto','300');
+insert into ingredient(id, ingredient,price) values ('4','funghi','300');
+insert into ingredient(id, ingredient,price) values ('5','salame piccante','400');
+insert into ingredient(id, ingredient,price) values ('6','tonno','200');
+insert into ingredient(id, ingredient,price) values ('7','cipolla','100');
+insert into ingredient(id, ingredient,price) values ('8','mortadella','400');
+insert into ingredient(id, ingredient,price) values ('9','pistacchi','500');
+insert into ingredient(id, ingredient,price) values ('10','burrata','500');
+insert into ingredient(id, ingredient,price) values ('11','salsiccia','400');
+insert into ingredient(id, ingredient,price) values ('12','friarielli','300');
+insert into ingredient(id, ingredient,price) values ('13','origano','200');
+insert into ingredient(id, ingredient,price) values ('14','speck','300');
+insert into ingredient(id, ingredient,price) values ('15','brie','300');
+insert into ingredient(id, ingredient,price) values ('16','olive','300');
+insert into ingredient(id, ingredient,price) values ('17','verdure grigliate','200');
+insert into ingredient(id, ingredient,price) values ('18','gorgonzola','300');
+insert into ingredient(id, ingredient,price) values ('19','fontina','300');
+insert into ingredient(id, ingredient,price) values ('20','pinoli','100');
+insert into ingredient(id, ingredient,price) values ('21','pesto','200');
+insert into ingredient(id, ingredient,price) values ('22','patate','200');
+
+
+insert into pizza(id, name) values ('1','marinara');
+insert into pizza(id, name) values ('2','margherita');
+insert into pizza(id, name) values ('3','prosciutto e funghi');
+insert into pizza(id, name) values ('4','4 formaggi');
+insert into pizza(id, name) values ('5','diavola');
+insert into pizza(id, name) values ('6','ligure');
+insert into pizza(id, name) values ('7','salsiccia e friarielli');
+insert into pizza(id, name) values ('8','vegetariana');
+insert into pizza(id, name) values ('9','tonno e cipolla');
+insert into pizza(id, name) values ('10','speck e brie');
+insert into pizza(id, name) values ('11','mortadella e pistacchi');
+
+
+
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('1','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('1','13');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('1','16');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('2','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('2','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('3','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('3','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('3','3');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('3','4');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('4','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('4','18');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('4','19');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('4','10');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('5','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('5','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('5','5');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('6','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('6','20');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('6','21');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('6','22');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('7','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('7','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('7','11');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('7','12');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('8','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('8','16');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('8','17');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('8','20');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('9','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('9','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('9','6');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('9','7');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('10','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('10','2');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('10','14');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('10','15');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('11','1');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('11','10');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('11','8');
+insert into pizza_ingredient(pizza_id,ingredient_id) values ('11','9');
+
+
--- app/MySQL/Pizza.sql
+++ app/MySQL/Pizza.sql
@@ -15,6 +15,52 @@ CREATE TABLE `pizza_ingredient` (
   PRIMARY KEY (`pizza_id`, `ingredient_id`)
 );
 
+CREATE TABLE `desserts` (
+  `id` int NOT NULL,
+  `name` varchar(255),
+  `price` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+
+CREATE TABLE `drinks` (
+  `id` int NOT NULL,
+  `name` varchar(255) ,
+  `price` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+CREATE TABLE `customers` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `postal_code` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+
+CREATE TABLE `riders` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `last_order` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `idcustomer` int NOT NULL,
+  `idrider` int NOT NULL,
+  `price` int NOT NULL,
+  `delivered` BOOLEAN DEFAULT NULL,
+  `discountCode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`,`idcustomer`),
+  KEY `idcustomer_idx` (`idcustomer`),
+  KEY `idrider_idx` (`idrider`),
+  CONSTRAINT `idcustomer` FOREIGN KEY (`idcustomer`) REFERENCES `customers` (`id`),
+  CONSTRAINT `idrider` FOREIGN KEY (`idrider`) REFERENCES `riders` (`id`)
+) ;
+
 ALTER TABLE `pizza_ingredient` ADD FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`);
 
 ALTER TABLE `pizza_ingredient` ADD FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`);
