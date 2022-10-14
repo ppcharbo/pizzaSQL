bede01489407c046c3f0dd8b1a18dea5b9ca55b6
Pierre Paul <pierrepaul.charbonnier@gmail.com>
2022-10-14 16:08:03 +0200

    database

--- app/MySQL/Pizza.sql
+++ app/MySQL/Pizza.sql
@@ -1,68 +1,86 @@
-CREATE TABLE `pizza` (
-  `id` int PRIMARY KEY,
-  `name` varchar(255)
-);
-
-CREATE TABLE `ingredient` (
-  `id` int PRIMARY KEY,
-  `ingredient` varchar(255),
-  `price` int
-);
-
-CREATE TABLE `pizza_ingredient` (
-  `pizza_id` int NOT NULL,
-  `ingredient_id` int NOT NULL,
-  PRIMARY KEY (`pizza_id`, `ingredient_id`)
-);
-
 CREATE TABLE `desserts` (
-  `id` int NOT NULL,
-  `name` varchar(255),
-  `price` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ;
-
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `price` double NOT NULL
+);
 
 CREATE TABLE `drinks` (
-  `id` int NOT NULL,
-  `name` varchar(255) ,
-  `price` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ;
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `price` double NOT NULL
+);
+
+CREATE TABLE `ingredients` (
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `price` double NOT NULL,
+  `veggie` tinyint NOT NULL
+);
+
+CREATE TABLE `pizzas` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) DEFAULT NULL
+);
 
 CREATE TABLE `customers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `address` varchar(255) NOT NULL,
-  `postal_code` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ;
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `passwd` varchar(45) NOT NULL
+);
 
+CREATE TABLE `menuitems` (
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `price` double NOT NULL
+);
 
 CREATE TABLE `riders` (
-  `id` int NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `last_order` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `available` tinyint NOT NULL DEFAULT "0",
+  `postal_code` int NOT NULL
+);
 
 CREATE TABLE `orders` (
   `id` int NOT NULL,
   `idcustomer` int NOT NULL,
   `idrider` int NOT NULL,
-  `price` int NOT NULL,
-  `delivered` BOOLEAN DEFAULT NULL,
-  `discountCode` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`,`idcustomer`),
-  KEY `idcustomer_idx` (`idcustomer`),
-  KEY `idrider_idx` (`idrider`),
-  CONSTRAINT `idcustomer` FOREIGN KEY (`idcustomer`) REFERENCES `customers` (`id`),
-  CONSTRAINT `idrider` FOREIGN KEY (`idrider`) REFERENCES `riders` (`id`)
-) ;
+  `price` double NOT NULL,
+  `ready_at` timestamp NOT NULL,
+  `picked_up_at` timestamp DEFAULT NULL,
+  `delivered` tinyint DEFAULT NULL,
+  `discount_code` varchar(10),
+  PRIMARY KEY (`id`, `idcustomer`, `discount_code`)
+);
 
-ALTER TABLE `pizza_ingredient` ADD FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`);
+CREATE TABLE `pizzas_ingredients` (
+  `pizzas_id` int,
+  `ingredients_id` int,
+  PRIMARY KEY (`pizzas_id`, `ingredients_id`)
+);
 
-ALTER TABLE `pizza_ingredient` ADD FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`);
+ALTER TABLE `pizzas_ingredients` ADD FOREIGN KEY (`pizzas_id`) REFERENCES `pizzas` (`id`);
 
+ALTER TABLE `pizzas_ingredients` ADD FOREIGN KEY (`ingredients_id`) REFERENCES `ingredients` (`id`);
+
+
+ALTER TABLE `drinks` ADD FOREIGN KEY (`id`) REFERENCES `menuitems` (`id`);
+
+ALTER TABLE `desserts` ADD FOREIGN KEY (`id`) REFERENCES `menuitems` (`id`);
+
+ALTER TABLE `pizzas` ADD FOREIGN KEY (`name`) REFERENCES `menuitems` (`id`);
+
+ALTER TABLE `riders` ADD FOREIGN KEY (`id`) REFERENCES `orders` (`idrider`);
+
+CREATE TABLE `customers_orders` (
+  `customers_id` int,
+  `orders_id` int,
+  PRIMARY KEY (`customers_id`, `orders_id`)
+);
+
+ALTER TABLE `customers_orders` ADD FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`);
+
+ALTER TABLE `customers_orders` ADD FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`);
 
